c0a0f604f6a54e5642d9d7270045a4c77bcb5ef9
Miguel Vazquez <miguel.vazquez@cnio.es>
2014-08-15 17:31:24 +0200

    nil

--- etc/app.d/post.rb
+++ etc/app.d/post.rb
@@ -1,18 +1,19 @@
 require 'rbbt/entity/study'
 
 require 'kramdown'
-def user_studies
-  @user_studies = Hash.new{ Sample.all_studies }
-end
 
 Workflow.require_workflow "ICGC"
 
+def user_studies
+  @user_studies = Hash.new{ Sample.all_studies.sort }
+end
+
+
 require 'rbbt'
 
-#path = Path.setup('', nil, nil, :local => Sample.study_repo.find["{PATH}"], :global => ICGC.root.find["{PATH}"]) 
-path = Path.setup('', nil, nil, :_local => Sample.study_repo.find["{PATH}"], :global => ICGC.root.find["{PATH}"]) 
-
-Sample.study_repo = path
+#path = Path.setup('', nil, nil, :_local => Sample.study_repo.find["{PATH}"], :global => ICGC.root.find["{PATH}"]) 
+#
+Sample.study_repo = ICGC.root.find
 
 module Sinatra
   module RbbtAuth
