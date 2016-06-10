2e9c2beea38e612e8f6f1dbe030f09f42cf0659c
Miguel Vazquez <miguel.vazquez@cnio.es>
2016-06-10 12:11:04 +0200

    nil

--- etc/app.d/pre.rb
+++ etc/app.d/pre.rb
@@ -2,5 +2,5 @@ require 'rbbt/workflow'
 
 Workflow.require_workflow "ICGC"
 Workflow.require_workflow "Study"
-Sample.study_repo = ICGC.root #.find
+Sample.study_repo = Path.setup('share/data/projects/ICGC.new')
 Sample.study_repo.resource = nil
